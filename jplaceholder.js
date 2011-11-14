be9269c7d34b797fdcf401cf4eb60b5d92dd4f72
lnv <lannyvo@gmail.com>
2011-11-14 15:38:50 -0600

    removed the support for placeholder fixed IE input pre-fill after the script executed fixed IE fired events twice for password input added callbacks for focus/blur/keypress

--- jplaceholder.js
+++ jplaceholder.js
@@ -11,7 +11,9 @@ $("form").jplaceholder(args);
 @param args {
 	force: boolean, // execute the this plug-in even the browser supports placeholder feature
 	showOnFocus: booealn // hide placeholder text when form input is focusing
-	
+	focusFn: function(obj, event) // callback function when input is focusing
+	keypressFn: function(obj, event) // callback function when a key is pressed
+	blurFn: function(obj, event) // callback function when input is blurred
 }
 */
 
@@ -21,24 +23,33 @@ $.fn.jplaceholder = function(args) {
     var el = document.createElement("input"),		
 		// default config
 		o = {
-			force: false,
-			showOnFocus: false
+			force: true,
+			showOnFocus: false,
+			focusFn: null,
+			keypressFn: null,
+			blurFn: null			
 		};					
 		
 		o = $.extend(o, args);   
-		
+	/*
     if (!o.force && "placeholder" in el) {
         return this;
     }
-	
-    $(this).find('input[placeholder!=""], textarea[placeholder!=""]').each(function (i) {
-        var t = $(this), 
-			p = t.parent(),				
-        	val = t.attr('placeholder'),
-			ph = $('<span class="ieplaceholder">' + val + '</span>'),
+	*/
+    $(this).find('input[type!="hidden"], textarea').each(function (i) {
+		var t = $(this);
+		
+		if (!t.attr('jplaceholder')) {return this;}
+          
+		var	p = t.parent(),				
+        	val = t.attr('jplaceholder'),
+			ph = $('<span class="jplaceholder">' + val + '</span>'),
 			isPassword = t.attr('type')=='password',
+			// make to use underscore for attribute name 
+			// ie font_family instead of font-family, it will be converted to font-family
 			css = {
 				position: 'absolute',
+				// feel to adjust the top & left in CSS file when the font is bigger or Arial
 				top: (t.offset().top - p.offset().top + 2) + 'px',
 				left: (t.offset().left - p.offset().left + 2) + 'px',
 				font_family: t.css('font-family'),
@@ -50,61 +61,63 @@ $.fn.jplaceholder = function(args) {
 			},		
 			timer;
 			
+		p.css('position','relative').css('zoom',1);
 
-		p.css('position','relative');
-		
-		if ($.browser.msie && parseInt($.browser.version, 10) == 7) {
-			p.css('zoom',1); // fixes ie7 box layout		
-		}
-		
-		t.attr('placeholder',''); // prevent conflict with browser supports placeholder
-							
-		
 		for (var k in css) {
 			ph.css(k.replace(/_/,'-'),css[k]);
 		}
+		
+		t.attr('placeholder',''); // prevent conflict with browser supports placeholder
+		t.css('visibility','visible');					
+		
 		 
 		ph.insertAfter(this).hide();  
 		
-		if (t.val()=='') {
-			ph.show();	
-		} 
-		
-		
-		t.focus(function(e){						
-			if (!o.showOnFocus) {
-				ph.hide();
-				return true;	
-			}			
-			
-			ph.addClass('focus');
-			
-			if (isPassword) {			
-				timer = setInterval(function(){
-					if (t.val()!='') {
-						ph.hide();
-						timer = clearInterval(timer);
-					};
-				}, 50);
-			} else {			
-				t.keypress(function(e){
-					ph.hide();
-				});
+		// fix ie prefill data after jplaceholder executed
+		setTimeout(function() {
+			if (t.val()=='') {				
+				ph.show();
 			}
+		}, 50); 
+		
+		
+		
+		function onfocus(e){									
+			if ((!o.showOnFocus) || (t.val()!='')) {
+				ph.fadeTo('fast', 0);			
+			} else {								
+				ph.addClass('focus').fadeTo('fast',0.5);
+			}
+			o.focusFn ? o.focusFn(t, e) : null;
+		}
+		
+		
+		ph.click(function(){
+			// get around ie fire event twice for password
+			if ( $.browser.msie && isPassword) {
+				t.select();	
+			} else {
+				t.focus();
+			}
+		});
+		t.focus(function(e){
+			onfocus(e);
 		}); // focus
+		t.keypress(function(e){
+			ph.fadeTo('fast', 0);
+			o.keypressFn ? o.keypressFn(t, e) : null;
+		});							
 
-		t.blur(function(e){
-			if (timer) {
-				clearInterval(timer);	
-			}
+		t.blur(function(e){			
 			if (t.val()=='') {
-				ph.show().removeClass('focus');
+				ph.fadeTo('fast', 1).removeClass('focus');
 			}
+			o.blurFn ? o.blurFn(t, e) : null;
 		}); //blur
  			
 	}); // find().each
 	
 	return this;
-};// ieplaceholder
+};// jplaceholder
 
 })(jQuery);
